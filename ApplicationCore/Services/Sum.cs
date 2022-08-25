cc50db43b010fa603ff9ccc40c6c6dcf093e9210
AngelJava78 <angel_java@hotmail.com>
2022-08-25 16:38:46 -0500

    UnitTest1 SumValues

---
+++ ApplicationCore/Services/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class Sum
+    {
+        public int SumValues(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ UnitTests/Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        [DataRow(5,2,7)]
+        public void SumValues(int num1, int num2, int expectedResult)
+        {
+            //
+            // TODO: Add test logic here
+            //
+            var sum = new Sum();
+            var result = sum.SumValues(num1, num2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
